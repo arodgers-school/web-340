504665238b979c7d5137bdf3b2e0836fbc67c3c7
Adam School <adrodgers@my365.bellevue.edu>
2021-08-08 10:54:48 -0500

    Exercise 1.3

--- week-1/rodgers-exercise-1.3.js
+++ week-1/rodgers-exercise-1.3.js
@@ -1,3 +1,13 @@
+/*
+Title: Exercise 1.3
+Author: Adam Rodgers
+Date: 8 Aug 2021
+Modified By: Adam Rodgers
+Description: Modules
+Resources:
+
+*/
+
 var url = require('url');
 
 var parsedURL = url.parse('https://www.example.com/profile?name=rodgers');
