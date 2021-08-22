0ae930524bd6887c7af7ec666b75730d7b0888a7
Adam School <adrodgers@my365.bellevue.edu>
2021-08-22 18:51:12 -0500

    Assignment 2.2

--- week-2/hello-world/rodgers-hello-world.js
+++ week-2/hello-world/rodgers-hello-world.js
@@ -1,11 +1,15 @@
+// Load Express and http module and define app variable
 var express = require("express");
 var http = require("http");
 var app = express();
 
+// Use express to look for requests and provide a response
 app.use(function (request, response) {
+  // Log request to console
   console.log("In comes a request to: " + request.url);
-
+  // Respond to request
   response.end("Hello World");
 });
 
+// Open server listening on port 8080
 http.createServer(app).listen(8080);
