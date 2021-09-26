1873467cf93c527c0a8c2d04fef375fe38521331
Adam School <adrodgers@my365.bellevue.edu>
2021-09-26 10:42:45 -0500

    Exercise 7.2

--- week-7/tdd-example/test/rodgers-exercise-7.2.js
+++ week-7/tdd-example/test/rodgers-exercise-7.2.js
@@ -1,5 +1,7 @@
+// Requires
 var assert = require("assert");
 
+// Mocha test for array with chai 'assert' function
 describe("String#split", function () {
   it("should return an array of fruits", function () {
     assert(Array.isArray("Apple,Orange,Mango".split(",")));
